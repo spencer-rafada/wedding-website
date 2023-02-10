1ca98dafbc82b403e3a09aaf775fb70a8d18bab4
spencer-rafada <spencer.byui@gmail.com>
2023-02-10 10:32:52 -0700

    added question card for a different feature

--- src/routes/FAQs/FAQ.jsx
+++ src/routes/FAQs/FAQ.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FaQuestion } from 'react-icons/fa';
+import QuestionCard from './components/QuestionCard'
 
 export default function FAQ() {
   return (
-    <div className="">
-      <h1>hello</h1>
+    <div className="faqContainer">
+      <div className="faqHeader">
+        <FaQuestion />
+        <h1>FAQ</h1>
+      </div>
+      {/* FAQs from our side */}
+      {/* Map QuestionCard */}
+      <QuestionCard />
+      {/* Questions from others */}
+      {/* Map APPROVED QuestionCard */}
     </div>
   );
 }
